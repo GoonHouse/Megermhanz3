0a25ea754471e32bdffd886a7d2b99d745cbf279
saforer <saforer@gmail.com>
2015-05-31 16:09:15 -0400

    man adding weapons is like half/all the fun of this so far. Homing rockets (that don't home but they do veer off to infinity)
    Signed-off-by: saforer <saforer@gmail.com>

---
+++ Assets/Scripts/HomingBomb.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomingBomb : MonoBehaviour {
+
+	public float speed;
+
+	private float timer = 1;
+	
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+	}
+}
