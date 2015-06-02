56c968114d88b28719eb729cff393199c9b5c051
EntranceJew <EntranceJew@gmail.com>
2015-06-01 20:27:43 -0400

    Subclassed HomingBomb to Projectile.
    Removed redundant Hurt code from Enemy.

--- Assets/Scripts/Projectiles/HomingBomb.cs
+++ Assets/Scripts/Projectiles/HomingBomb.cs
@@ -1,19 +1,10 @@
 using UnityEngine;
 using System.Collections;
 
-public class HomingBomb : MonoBehaviour {
-
-	public float speed;
+public class HomingBomb : Projectile {
 
 	private float timer = 1;
-	
-	private Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D> ();
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		timer -= Time.deltaTime;
